c0cb62758efda9b48d78ace6002dd0af0c1d0f0b
istamarahsan <istamarahsan@gmail.com>
2023-09-02 09:27:25 +0700

    close WoW form

--- src/pages/daftar.tsx
+++ src/pages/daftar.tsx
@@ -18,81 +18,79 @@ export default function Daftar() {
                     Level Up with BGDC
                 </Typography>
                 <div className="flex flex-row flex-wrap justify-center gap-10">
-                    <Card className="flex min-h-[422px] w-[90vw] max-w-sm flex-col items-center gap-5 bg-white p-5 shadow-2xl">
-                        <div className="flex flex-col items-center gap-2">
-                            <Typography
-                                variant="h2"
-                                color="black"
-                                className="text-center font-serif text-2xl font-medium"
-                            >
-                                Week of Welcoming (B27)
-                            </Typography>
-                            <Alert
-                                className="p-2"
-                                color="blue-gray"
-                                icon={<ExclamationCircleIcon className="h-5 w-5" />}
-                            >
-                                <Typography variant="small" className="text-xs">
-                                    - Registering onsite? Please check your WoW schedule.<br/>
-                                    - {"Don't forget to join the "}
-                                    <a 
-                                    href="https://binusgdc.com/link/events"
-                                    target="_blank"
-                                    className="underline"
-                                    >
-                                        {"event server."}
-                                    </a>
+                    <Card className="relative min-h-[422px] w-[90vw] max-w-sm bg-white opacity-60 shadow-2xl">
+                        <Typography
+                            variant="lead"
+                            color="black"
+                            className="absolute top-1/2 w-full text-center font-serif text-4xl opacity-100"
+                        >
+                            CLOSED
+                        </Typography>
+                        <div className="flex flex-col items-center gap-5 p-5">
+                            <div className="flex flex-col items-center gap-2">
+                                <Typography
+                                    variant="h2"
+                                    color="black"
+                                    className="text-center font-serif text-2xl font-medium"
+                                >
+                                    Week of Welcoming (B27)
                                 </Typography>
-                            </Alert>
-                            <Alert
-                                className="p-2"
-                                color="red"
-                                icon={<WarningRounded className="h-5 w-5" />}
-                            >
-                                <Typography variant="small" className="text-xs">
-                                    Nomor rekening on the brochure is wrong. Please use the details below.
+                                <Alert
+                                    className="p-2"
+                                    color="blue-gray"
+                                    icon={<ExclamationCircleIcon className="h-5 w-5" />}
+                                >
+                                    <Typography variant="small" className="text-xs">
+                                        - Registering onsite? Please check your WoW schedule.
+                                        <br />- {"Don't forget to join the "}
+                                        <a
+                                            href="https://binusgdc.com/link/events"
+                                            target="_blank"
+                                            className="underline"
+                                        >
+                                            {"event server."}
+                                        </a>
+                                    </Typography>
+                                </Alert>
+                                <Alert
+                                    className="p-2"
+                                    color="red"
+                                    icon={<WarningRounded className="h-5 w-5" />}
+                                >
+                                    <Typography variant="small" className="text-xs">
+                                        Nomor rekening on the brochure is wrong. Please use the
+                                        details below.
+                                    </Typography>
+                                </Alert>
+                            </div>
+                            <div className="w-full">
+                                <Typography variant="small" className="my-1 w-full text-left">
+                                    Registration fee details:
                                 </Typography>
-                            </Alert>
-                        </div>
-                        <div className="w-full">
-                            <Typography variant="small" className="my-1 w-full text-left">
-                                Registration fee details:
-                            </Typography>
-                            <div className="flex w-fit flex-col items-stretch text-left">
-                                <div className="flex flex-col text-left">
-                                    <Typography variant="small">Nominal Transfer:</Typography>
-                                    <Typography variant="small" className="font-bold">
-                                        Rp 100.000
-                                    </Typography>
-                                </div>
-                                <div className="flex flex-col text-left">
-                                    <Typography variant="small">Nomor Rekening:</Typography>
-                                    <Typography variant="small" className="font-bold">
-                                        BCA 5272020647
-                                    </Typography>
-                                </div>
-                                <div className="flex flex-col text-left">
-                                    <Typography variant="small">Atas Nama:</Typography>
-                                    <Typography variant="small" className="font-bold">
-                                        IDA BAGUS K G P & MARY R
-                                    </Typography>
+                                <div className="flex w-fit flex-col items-stretch text-left">
+                                    <div className="flex flex-col text-left">
+                                        <Typography variant="small">Nominal Transfer:</Typography>
+                                        <Typography variant="small" className="font-bold">
+                                            Rp 100.000
+                                        </Typography>
+                                    </div>
+                                    <div className="flex flex-col text-left">
+                                        <Typography variant="small">Nomor Rekening:</Typography>
+                                        <Typography variant="small" className="font-bold">
+                                            BCA 5272020647
+                                        </Typography>
+                                    </div>
+                                    <div className="flex flex-col text-left">
+                                        <Typography variant="small">Atas Nama:</Typography>
+                                        <Typography variant="small" className="font-bold">
+                                            IDA BAGUS K G P & MARY R
+                                        </Typography>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <Button
-                            size="sm"
-                            className="flex flex-row items-center gap-2"
-                            onClick={() => {
-                                void navigator.clipboard.writeText("5272020647")
-                                toast.success(`Copied "5272020647"`)
-                            }}
-                        >
-                            <FaCopy /> Copy Nomor Rekening
-                        </Button>
-                        <a
-                            href="https://bit.ly/StudentOrganizationsRegistrationB2027"
-                            target="_blank"
-                        >
+                            <Button size="sm" className="flex flex-row items-center gap-2">
+                                <FaCopy /> Copy Nomor Rekening
+                            </Button>
                             <Button
                                 color="deep-purple"
                                 size="lg"
@@ -100,7 +98,7 @@ export default function Daftar() {
                             >
                                 Registration Form B27 <ArrowUpRightIcon className="h-4 w-4" />
                             </Button>
-                        </a>
+                        </div>
                     </Card>
                     <Card className="relative flex min-h-[450px] w-[90vw] max-w-sm flex-col items-center gap-5 bg-white p-5 opacity-60 shadow-2xl">
                         <Typography
